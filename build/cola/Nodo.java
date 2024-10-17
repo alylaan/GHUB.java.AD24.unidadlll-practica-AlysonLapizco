5ab1858e32391b7781d6a5960d488fbde025b58e
Alyson Lapizco <160778240+alylaan@users.noreply.github.com>
2024-10-17 15:47:08 +0000

    nodo

--- build/cola/Nodo.java
+++ build/cola/Nodo.java
@@ -1,5 +1,25 @@
 package cola;
 
 public class Nodo{
+    private T valor;
+    private Nodo<T> siguiente;
+
+    public Nodo() {
+        this.valor = null;
+        this.siguiente = null;
+    }
+    public T getValor(){
+        return valor;
+    }
+    public void setValor(T valor){
+        this.valor = valor;
+    }
+    public nodo<T> getSiguiente(){
+        return siguiente;
+    }
+    public void setSiguiente(Nodo<T> siguiente){
+        this.siguiente = siguiente;
+    }
+
     
 }
